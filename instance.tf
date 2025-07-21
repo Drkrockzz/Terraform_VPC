28dc4ef2d5a21264ed155d615fae636e8690edc9
MD Danish Khan <MK00861532@techmahindra.com>
2025-07-21 18:51:30 +0530

    updating userdata

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "nginx_pub" {
   ami = "ami-0d03cb826412c6b0f"
   instance_type = "t2.micro"
   subnet_id =  aws_subnet.custom-pub-subnet.id
-  user_data_base64 = file("userdata.sh")
+  user_data = file("userdata.sh")
   security_groups = [ aws_security_group.custom-vpc-sg.id ]
   key_name = "mumbai_key"
   tags = {
